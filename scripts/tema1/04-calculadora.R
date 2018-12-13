da3d0abb6fb69b01a2a7be4d3691573ca06f232e
Juan Gabriel <juangabriel@frogames.es>
2018-12-13 19:04:04 +0100

    Completado tema 1 scripts

--- scripts/tema1/04-calculadora.R
+++ scripts/tema1/04-calculadora.R
@@ -126,3 +126,118 @@ round(5, sqrt(2))
 floor(-3.45)
 ceiling(-3.45)
 trunc(-3.45)
+
+
+x = (pi^2)/2
+x
+
+y <- cos(pi/4)
+y
+
+sin(pi/4) + cos(pi/4) -> z
+z
+
+edad <- 30
+
+nombre = "Juan Gabriel"
+
+HOLA = 1
+hola = 5
+
+pi.4 = 4*pi
+pi.4
+
+x = 2
+x = x^2
+
+x = sqrt(x)
+
+## Función f(x) = x^3 - (3^x)* sen(x)
+f = function(x) {
+  x^3 - (3^x) * sin(x)
+}
+
+f(4) # 4^3 - 3^4 * sin(4)
+f(5)
+f(pi/2)
+
+suma1 <- function(t){
+  t + 1
+}
+suma1(6)
+suma1(-5)
+
+product <- function(x, y){
+  x*y
+}
+product(5,7)
+
+g <- function(x,y,z) {
+  exp(x^2 + y^2) * sin(z)
+}
+g(1, 2, 3)
+g(1, -1, pi)
+
+suma5 <- function(numero){
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  numero = suma1(numero);
+  suma1(numero)
+}
+
+suma5(3)
+
+ls()
+
+rm(product)
+
+ls()
+
+rm(list = ls())
+
+class(3+2i)
+
+(3+2i)*5
+
+(3+2i)*(-1+3i)
+
+(3+2i)/(-1+3i)
+
+#Esto es un error:
+#2+7*i
+
+#pi + sqrt(2)i
+complex(real = pi, imaginary = sqrt(2)) -> z1
+z1
+
+sqrt(-5)
+sqrt(as.complex(-5))
+
+#La raíz cuadrada devuelve, de las dos soluciones la de
+#Re(z)>0, para obtener la otra,  hay que multiplicar por -1
+sqrt(3+2i) # z^2 = 3+2i
+exp(3+2i)
+sin(3+2i)
+cos(3+2i)
+
+#Módulo = sqrt(Re(z)^2 + Im(z)^2)
+Mod(z1)
+#Argumento = arctan(Im(z)/Re(z))
+# = arccos(Re(z)/Mod(z))
+# = arcsin(Im(z)/Mod(z))
+# va de (-pi, pi]
+Arg(-1+0i)
+Arg(-1-2i)
+#Conjugado = Re(z)- Im(z)i
+Conj(z1)
+#Parte Real y Parte Imaginaria
+Re(z1)
+Im(z1)
+
+### z = Mod(z) * (cos(Arg(z))+sin(Arg(z))i)
+complex(modulus = 2, argument = pi/2) -> z2
+z2
+Mod(z2)
+Arg(z2)
+pi/2
