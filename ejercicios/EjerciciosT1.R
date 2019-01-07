fda64351916b4dc1721c3d1a9f865cfbe81ae2bc
msantos <maria@frogames.es>
2019-01-07 18:50:09 +0100

    Updates Ejercicio y Tema 11

---
+++ ejercicios/EjerciciosT1.R
@@ -0,0 +1,15 @@
+opBasic = function(a,b){
+  print("Suma")
+  print(a+b)
+  print("Resta")
+  print(paste(sprintf("%i - %i = ",a,b),a-b))
+  print(paste(sprintf("%i - %i = ",b,a),b-a))
+  print("Producto")
+  print(a*b)
+  print("Cociente de la división entera")
+  print(paste(sprintf("%i : %i = ",a,b),a%/%b))
+  print(paste("con resto ",a%%b))
+  print("Cociente de la división entera")
+  print(paste(sprintf("%i : %i = ",b,a),b%/%a))
+  print(paste("con resto ",b%%a))
+}
