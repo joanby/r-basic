8205f832b782ed662bfb1be6e669e1f5edf9141d
mrbyron911 <mr_byron911@hotmail.com>
2024-11-04 23:58:05 -0400

    R como calculadora(sqrt, exp, abs, factorial, permutación, trigonometría)

---
+++ scripts/tema1/05-Mi prueba R como calculadora.R
@@ -0,0 +1,29 @@
+2*(3+5/2)
+1+1 #Suma
+
+2*3+1) #se debe completar los simbolos como ejemplo, falta el '(' de apertura.
+
+5-3 #Resta
+8*3 #Multiplicación
+
+2^5 #Potencia
+
+5/2 #Division
+7%/%2 #Solo muestra la parte entera del cociente.
+7%%2 #Muestra el residuo o módulo
+
+30%%7
+30%/%7
+31%%7
+
+pi #pi
+pi*2
+2*pi/2
+
+Inf # Es el número más grande que existe
+-Inf #-Infinito es el número más pequeño que existe
+
+5/0 # El resultado es Inf
+0/0 # El resultado es un NaN (Not a Number o Indeterminado)
+
+NA #Valor desconocido (Not Available)
